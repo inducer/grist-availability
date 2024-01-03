6a73a4b43c2db62a67becbd82867515656597413
Andreas Kloeckner <inform@tiker.net>
2024-01-03 15:48:20 -0600

    Update dev doc id

--- dev.sh
+++ dev.sh
@@ -2,6 +2,6 @@
 
 export GRIST_ROOT_URL="https://grist.tiker.net"
 export GRIST_API_KEY_FILE="$HOME/.grist-api-key"
-export GRIST_DOC_ID="rLJPGJ9RLJ4TRVx4AxT2tW"
+export GRIST_DOC_ID="isSNVL12chULhkfCZkRbus"
 
 poetry run flask --app=availability.app run --debug
